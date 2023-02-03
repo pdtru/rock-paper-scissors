16b34c6c93239e029a698aaf9105ad76ba5caba5
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-02-02 16:00:49 -0800

    Add game function in app.js

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-const choices = ['rock', 'paper', 'scissors'];
-
-// function that selects random element in an array
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-// case-insensitive prompt for player
-const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
-
 // function for one round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection === computerSelection) {
         return "It's a tie!"
 
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "Rock beats scissors. You win!"
+        return "You win! Rock beats scissors."
 
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "Paper beats rock. You lose!"
+        return "You lose! Paper beats rock."
 
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "Paper beats rock. You win!"
+        return "You win! Paper beats rock."
 
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return "Scissors beats paper. You lose!"
+        return "You lose! Scissors beats paper."
 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "Scissors beats paper. You win!"
+        return "You win! Scissors beats paper."
 
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "Rock beats scissors. You lose!"
+        return "You lose! Rock beats scissors. "
 
     } else {
-        return "Invalid option. Please choose rock, paper or scissors."
+        return "Invalid choice. Please choose rock, paper or scissors."
     }
 }
 
-console.log("Player chose: " + playerSelection);
-console.log("Computer chose: " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+const choices = ['rock', 'paper', 'scissors'];
+
+function game() {
+    for (let i = 1; i < 6; i++) {
+        const roundNumber = i;
+        alert("Round " + roundNumber);
+        // case-insensitive prompt for user
+        const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+        // variable that selects random value from an array
+        const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+        const result = playRound(playerSelection, computerSelection);
+        console.log("Player chose: " + playerSelection);
+        console.log("Computer chose: " + computerSelection);
+        console.log(result);
+    }
+}
+game();
