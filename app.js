84492c6683b64dec120aa91cfb7ae7af290e9463
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-03-15 14:38:25 -0700

    Add changing image for user

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+const you_div = document.getElementById("you");
+const cpu_div = document.getElementById("cpu");
 
 function getcomputerChoice() {
     const choices = ['r', 'p', 's'];
@@ -14,22 +16,40 @@ function getcomputerChoice() {
     return choices[randomNumber]; a
 }
 
+function convertToWord(userChoice) {
+    if (userChoice == "r") return "ROCK";
+    if (userChoice == "p") return "PAPER";
+    return "SCISSORS";
+}
+
+function changeImageRock() {
+    you_div.src="images/rock.png";
+}
+
+function changeImagePaper() {
+    you_div.src="images/paper.png";
+}
+
+function changeImageScissors() {
+    you_div.src="images/scissors.png";
+}
+
 function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!"
+    result_p.innerHTML = `${convertToWord(userChoice)} BEATS ${convertToWord(computerChoice)}. YOU WIN!`
 }
 
 function lose(userChoice, computerChoice) {
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!"
+    result_p.innerHTML = `${convertToWord(computerChoice)} BEATS ${convertToWord(userChoice)}. YOU LOSE!`
 }
 
 function draw(userChoice, computerChoice) {
-    result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!"
+    result_p.innerHTML = "DRAW!"
 }
 
 function game(userChoice) {
@@ -38,17 +58,17 @@ function game(userChoice) {
         case "rs":
         case "pr":
         case "sp":
-            win();
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            lose();
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            draw();
+            draw(userChoice, computerChoice);
             break;
     }
 }
@@ -56,12 +76,15 @@ function game(userChoice) {
 function main() {
     rock_div.addEventListener('click', function () {
         game("r");
+        changeImageRock();
     })
     paper_div.addEventListener('click', function () {
         game("p");
+        changeImagePaper();
     })
     scissors_div.addEventListener('click', function () {
         game("s");
+        changeImageScissors();
     })
 }
 
